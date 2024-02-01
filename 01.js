5f64e21f03ea1eb81ffcff11d0d3da2d03ec88fd
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-01 12:46:39 +0300

    First steps on FCC

---
+++ 01.js
@@ -0,0 +1,10 @@
+//1
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// 2
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
