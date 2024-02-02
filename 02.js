8f9c4f441c094406f8f5c2901005143304d0d686
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-02 14:28:51 +0300

    Second part of tasks

---
+++ 02.js
@@ -0,0 +1,23 @@
+//1
+const sum = 10 + 0;
+
+//2
+const difference = 45 - 33;
+
+//3
+const product = 8 * 10;
+
+//4
+const quotient = 66 / 33;
+
+//5
+let myVar = 87;
+myVar++;
+
+//6
+let myLet = 11;
+myLet--;
+
+//7
+const ourDecimal = 5.7;
+let myDecimal = 10.5;
