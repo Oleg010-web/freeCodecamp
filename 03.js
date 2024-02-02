6010295873c1d07dfbab6aac55727de138498de5
Oleg010-web <nesteroff_oleg@mail.ru>
2024-02-02 14:57:38 +0300

    Third part of tasks

---
+++ 03.js
@@ -0,0 +1,42 @@
+//1
+const product = 2.0 * 2.5;
+//2
+const quotient = 4.4 / 2.0;
+//3
+const remainder = 11 % 3;
+//4
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a+=12;
+b+=9;
+c+=7;
+//5
+let d = 11;
+let t = 9;
+let j = 3;
+
+// Only change code below this line
+d-=6;
+t-=15;
+j-=1;
+//6
+let jt = 5;
+let jy= 12;
+let ji = 4.6;
+
+// Only change code below this line
+jt *= 5;
+jy *= 3;
+ji *= 10;
+//7
+let jm = 48;
+let jn = 108;
+let jb = 33;
+
+// Only change code below this line
+jm /= 12;
+jn /= 4;
+jb /= 11;
